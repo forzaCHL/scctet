1964f7bde32df117c85d0168e21d9b08ff61e61b
陈海林 <chenhailin@zhixueyun.com>
2019-01-23 14:27:31 +0800

    new file

---
+++ scctet/demo.h
@@ -0,0 +1,17 @@
+//
+//  demo.h
+//  scctet
+//
+//  Created by Apple on 2019/1/23.
+//  Copyright © 2019年 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface demo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ scctet/demo.m
@@ -0,0 +1,13 @@
+//
+//  demo.m
+//  scctet
+//
+//  Created by Apple on 2019/1/23.
+//  Copyright © 2019年 Apple. All rights reserved.
+//
+
+#import "demo.h"
+
+@implementation demo
+
+@end
